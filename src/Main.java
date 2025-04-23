0ae3353e8f814e1e68f591476edebea91f24da40
ABargybaev <azamatbargybaev@gmail.com>
2025-04-23 14:37:11 +0600

    Database added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:postgresql://localhost:5432/chocoshop";
+        String user = "postgres";
+        String password = "AzamatSQL69420";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("✅ Успешное подключение!");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT version();");
+            if (rs.next()) {
+                System.out.println("PostgreSQL: " + rs.getString(1));
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения:");
+            e.printStackTrace();
+        }
     }
 }
