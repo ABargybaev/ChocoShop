00d81dab685edf2fcb23dedb2c12a89f18dc5c10
ABargybaev <azamatbargybaev@gmail.com>
2025-05-10 19:55:43 +0600

    Updated menu with switch case

--- src/chocoshop/Main.java
+++ src/chocoshop/Main.java
@@ -4,25 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Menu menu = new Menu();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\nWelcome to Chocolate Shop system! List of actions:\n \n 1) Make a trade\n 2) Add a new type of chocolate\n 3) Show all suppliers\n 4) Add a new supplier\n 5) Exit\n\n Choose your action: ");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\nWelcome to Chocolate Shop system!");
+
         while (true) {
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-            if (choice == 1) {
-                menu.Trade();
-            }
-            ///testing area///
-            else if (choice == 69) {
-                menu.showAllChocolates();
-            }
-            else if (choice == 68) {
-                menu.showOneChocolate();
-            }
-            else if (choice == 100) {
-                menu.recoverQuantity();
+            System.out.print("\nList of actions:\n \n 1) Make a trade\n 2) Add a new type of chocolate\n 3) Show all suppliers\n 4) Add a new supplier\n 5) Exit\n\n Choose your action: ");
+            int choice = scan.nextInt();
+            scan.nextLine();
+            System.out.print("\n");
+            switch (choice) {
+                case 1:
+                    menu.Trade();
+                    break;
+                    /////testing area/////
+                case 69:
+                    menu.showAllChocolates();
+                    break;
+                case 68:
+                    menu.showOneChocolate();
+                    break;
+                case 100:
+                    menu.recoverQuantity();
+                    break;
+                case 5:
+                    System.out.println("Come again!\nExiting program...");
+                    return;
+                default:
+                    System.out.println("Try again!");
             }
         }
+
     }
 }
